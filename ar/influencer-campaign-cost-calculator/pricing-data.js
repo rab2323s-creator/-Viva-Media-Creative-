73cecebd4a48a7ef4876e16dc865080aacc58a23
rab2323s-creator <rab2323s@gmail.com>
2026-06-16 23:02:54 +0200

    Create pricing-data.js

---
+++ ar/influencer-campaign-cost-calculator/pricing-data.js
@@ -0,0 +1,31 @@
+const CURRENCY = {
+  saudi: {
+    code: "SAR",
+    symbol: "ر.س",
+    name: "السعودية"
+  },
+
+  uae: {
+    code: "AED",
+    symbol: "د.إ",
+    name: "الإمارات"
+  },
+
+  kuwait: {
+    code: "KWD",
+    symbol: "د.ك",
+    name: "الكويت"
+  },
+
+  qatar: {
+    code: "QAR",
+    symbol: "ر.ق",
+    name: "قطر"
+  },
+
+  egypt: {
+    code: "EGP",
+    symbol: "ج.م",
+    name: "مصر"
+  }
+};
